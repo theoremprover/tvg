4042f56b22cf34873f5da47d418c6273dd4cbb33
Robert <no.email@given.de>
2018-10-12 16:03:22 +0200

    No Message

--- test.c
+++ test.c
@@ -24,29 +24,46 @@ void addCombination(ULONG cur_combination,UBYTE* p_num_combinations,ULONG* combi
 	combinations[(*p_num_combinations)++] = cur_combination;
 }
 
+
+DECISION decision1234 = {
+	0, {0L,0L,0L,0L,0L,0L,0L,0L,0L,0L,0L,0L,0L,0L,0L,0L,0L},
+	0, {0L,0L,0L,0L,0L,0L,0L,0L,0L,0L,0L,0L,0L,0L,0L,0L,0L},
+	0L };
+DECISION all_decisions[1] = { &decision1234 };
+
+int combs[] = { 
+
 int main(int argc,char *argv[])
 {
 	if(argc<4) return(1);
 
-	decision1234_cond[0] = (int)strcmp(argv[1],"1")==0;
-	if(a)   // Lazy "||" operator
+	for(int i=0;i<10;i++)
 	{
-		decision1234_cond[1] = (int)strcmp(argv[2],"1");
-		// Strict "&" operator, so no "if"
-		decision1234_cond[2] = (int)strcmp(argv[3],"1");
-	}
-	if(decision1234_cond[0]) decision1234_cond_True[0]++; else decision1234_cond_False[0]++;
-	if(decision1234_cond[1]) decision1234_cond_True[1]++; else decision1234_cond_False[1]++;
-	if(decision1234_cond[2]) decision1234_cond_True[2]++; else decision1234_cond_False[2]++;
-	if((strcmp(argv[1],"1")==0) || (strcmp(argv[2],"1") & strcmp(argv[3],"1")))
-	{
-		decision1234_True++;
-		printf("True\n");
-	}
-	else
-	{
-		decision1234_False++;
-		printf("False\n");
+		int a,b,c;
+		a = combs[i*3];
+		if(a)   // Lazy "||" operator, evaluate b and c only if a==True
+		{
+			add
+			b = combs[i*3+1];
+			// Strict "&" operator, hence evaluating c unconditionally
+			c = combs[i*3+2];
+		}
+		else
+		{
+			// b and c not evaluated, could be any 
+		}
+
+		ULONG cur_comb = (a&1)<<0 | (b&1)<<1 | (c&1)<<2;
+		if(a || (b & c))
+		{
+			decisionOutcome(cur_comb,&decision1234,True);
+			printf("True\n");
+		}
+		else
+		{
+			decisionOutcome(cur_comb,&decision1234,True);
+			printf("False\n");
+		}
 	}
 
 	return(0);
