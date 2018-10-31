3d5b3cae2c07a9b01558fdb90cdc105c89a4c287
Robert <bla@bla.com>
2018-10-31 15:53:38 +0100

    No Message

--- covreport/Main.hs
+++ covreport/Main.hs
@@ -29,32 +29,30 @@ main = do
 		putStrLn $ show n ++ " / " ++ show (length cov) ++ " : Creating " ++ sourceFilenameS ++ " .."
 		srctext <- readFile sourceFilenameS
 		let srcfilename = show n <.> "html"
-		writeFile (coverage_dir </> srcfilename) $ renderHtml $ annotateSrc srcfile srctext
+		writeFile (coverage_dir </> srcfilename) $ renderHtml $ colourSrc srcfile srctext
 
 		let (n_cov,n_stmts,cov_pct,_) = srcfile_covs srcfile
 		return $ [ stringToHtml (show n), (anchor (stringToHtml sourceFilenameS)) ! [href srcfilename], stringToHtml (printf "%5.1f %%" cov_pct) ]
 
 	writeFile (coverage_dir </> "index.html") $ renderHtml $ simpleTable [] [] indexlines
 
-annotateSrc SrcFile{..} srctext = (font ! [ face "courier" ]) $ pre $
-	insert_fontcols countersS $ zip3 [1..] (repeat 1) (lines srctext)
-	where
-	insert_fontcols [] _ = []
-	insert_fontcols (Counter{..}:cnts) srctext = stringToHtml (take (
+data SrcPart = PlainText Int Int String | Colouring String SrcPart
 
+colourSrc SrcFile{..} srctext = (font ! [ face "courier" ]) $ pre $ concatHtml $ map to_html $
+	search_srcpart (sort countersS) $ map PlainText $ zip3 [1..] (repeat 1) (map (++"\n") $ lines srctext)
+
+	to_html (_,_,txt) = toHtml txt
+	to_html NewLine = toHtml "\n"
+	to_html (Colouring colour srcparts) = (font ! [ thestyle $ "background-color:" ++ colour ]) $ concatHtml $ map to_html srcparts
+
+	search_srcpart Counter{..} ((PlainText l c txt):srcparts) | l == lineC && c <= columnC = let
+		(middles,last_part) = find_last (lineC,columnC) srcparts
+	
+	&& c + length txt > columnC =
+		PlainText l c (take (columnC-c) txt) : Colouring (take lenC $ drop (columnC-c) txt) (if cntC==0 then "#ff0000" else "#00ff00") : PlainText l (columnC+lenC) (drop (columnC+lenC) txt) : srcparts
+	mb_insert_colouring
+	
 {-
 data Counter = Counter { lineC :: Int, columnC :: Int, lenC :: Int, cntC :: Int } deriving (Eq,Ord,Show,Generic)
 data SrcFile = SrcFile { sourceFilenameS :: String, outputFilenameS :: String, countersS :: [Counter] } deriving (Show,Generic)
-
-annotateSrc SrcFile{..} srclines = (font ! [ face "courier" ]) (simpleTable [] [bgcolor "#f0f0f0"] (map annotate_line (zip [1..] srclines)))
-	where
-	replace_tabs txt = map (\case '\t' -> ' '; c -> c) txt
-	annotate_line (line_no,line) = [ stringToHtml (show line_no),
-		concatHtml $ set_bgcol (replace_tabs line) 1 (sort [ cnt | cnt@Counter{..} <- countersS, lineC==line_no ]) ]
-	set_bgcol (' ':line) cursor rs = spaceHtml : set_bgcol line (cursor+1) rs
-	set_bgcol line _ [] = [stringToHtml line]
-	set_bgcol line cursor (cnt@Counter{..}:rs) | cursor>=columnC = (cnt_font cnt) (stringToHtml (take lenC line)) : set_bgcol (drop lenC line) (cursor+lenC) rs
-	set_bgcol line cursor rs@(Counter{..}:_) = stringToHtml (take (columnC-cursor) line) : set_bgcol (drop (columnC-cursor) line) columnC rs
-	cnt_font cnt@Counter{..} = font ! [ thestyle $ "background-color:" ++ (if cntC==0 then "#ff0000" else "#00ff00"),
-		title $ show cnt]
 -}
