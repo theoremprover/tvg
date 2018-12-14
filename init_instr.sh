76238e75d2528579b49936cf4bbffa8ac22d9c99
3T <none@none.com>
2018-12-15 00:21:00 +0100

    No Message

--- init_instr.sh
+++ init_instr.sh
@@ -15,7 +15,6 @@ source /tvg/tvg/make_tvg.sh
 rm -f cov.dat
 
 cd /tvg/build
-#../gcc-4.7.4/configure --disable-checking --enable-languages=c --disable-multiarch --disable-multilib --enable-shared --enable-threads=posix --program-suffix=-instr --with-gmp=/usr --with-mpc=/usr/lib --with-mpfr=/usr/lib --without-included-gettext --with-system-zlib --with-tune=generic --prefix=/tvg/install/gcc-4.7.4 --disable-bootstrap --disable-build-with-cxx
 ../gcc-4.7.4/configure --disable-checking --enable-languages=c --disable-multiarch --disable-multilib --enable-shared --enable-threads=posix --program-suffix=-instr --with-gmp=/usr/local --with-mpc=/usr/local/lib --with-mpfr=/usr/local/lib --without-included-gettext --with-system-zlib --with-tune=generic --prefix=/tvg/install/gcc-4.7.4 --disable-bootstrap --disable-build-with-cxx
 
 make
@@ -34,3 +33,5 @@ stack install --allow-different-user --ghc-options -O3 --force-dirty
 stack --allow-different-user --stack-yaml /tvg/tvg/stack.yaml ghc -- -shared -threaded -dynamic -fPIC -no-hs-main -I/tvg/tvg/incs /tvg/tvg/incs/data.c /tvg/tvg/incs/CovStats.hs -o /tvg/tvg/incs/libdata.so -lHSrts_thr-ghc8.4.3 -lffi
 
 popd
+
+# export C_INCLUDE_PATH=/usr/include/x86_64-linux-gnu
