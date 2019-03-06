aecb0e8c4f804fe51a364ed9ddc296bce62613a4
3T <none@none.com>
2019-03-06 18:01:41 +0100

    No Message

--- grammar/DataTree.hs
+++ grammar/DataTree.hs
@@ -6,53 +6,51 @@ module DataTree (toDataTree) where
 import GHC.Generics
 import Language.C
 import Language.C.Data.Ident
-	
+import Text.Html
 
-data DataTree = DataTree [DataTree] | Leaf (Show v) deriving (Show)
+
+data DataTree = DataTree String [DataTree] | Leaf String deriving (Show)
 
 class DataTreeNode f where
 	dataTree :: f p -> DataTree
 
 instance DataTreeNode V1 where
-	dataTree _ _ = error "DataTreeNode of empty type!"
+	dataTree _ = error "DataTreeNode of empty type!"
 
 instance DataTreeNode U1 where
-	dataTree _ _ = Unit
+	dataTree _ = Leaf "()"
 
 instance {-# OVERLAPS #-} DataTreeNode (K1 i NodeInfo) where
-	dataTree (K1 c) = ind i ++ show c ++ "\n"
+	dataTree (K1 c) = Leaf $ show c
 
 instance {-# OVERLAPS #-} DataTreeNode (K1 i Ident) where
-	dataTree (K1 (Ident name _ _)) = ind i ++ "Ident " ++ show name ++ "\n"
+	dataTree (K1 (Ident name _ _)) = "Ident " ++ show name ++ "\n"
 
-instance {-# OVERLAPS #-} DataTreeNode (K1 i Int) where
-	dataTree (K1 c) = show c ++ "\n"
-instance {-# OVERLAPS #-} DataTreeNode (K1 i Char) where
-	dataTree (K1 c) = show c ++ "\n"
-instance {-# OVERLAPS #-} DataTreeNode (K1 i String) where
-	dataTree (K1 c) = show c ++ "\n"
-instance {-# OVERLAPS #-} DataTreeNode (K1 i Integer) where
-	dataTree (K1 c) = show c ++ "\n"
+instance {-# OVERLAPS #-} (Show t) => DataTreeNode (K1 i t) where
+	dataTree (K1 c) = Leaf $ show c
 
 instance (Generic c,DataTreeNode (Rep c)) => DataTreeNode (K1 i c) where
-	dataTree (K1 c) = DataTreeNode (from c)
+	dataTree (K1 c) = dataTree (from c)
 
 instance (DataTreeNode f,Constructor c) => DataTreeNode (M1 C c f) where
 	dataTree (M1 x) = case conName (undefined :: M1 C c f p) of
-		conname -> conname ++ "\n" ++ DataTreeNode (i+1) x
+		conname -> DataTree conname [dataTree x]
 
 instance (DataTreeNode f,Selector s) => DataTreeNode (M1 S s f) where
-	dataTree (M1 x) = DataTreeNode x
+	dataTree (M1 x) = dataTree x
 
 instance (DataTreeNode f,Datatype d) => DataTreeNode (M1 D d f) where
-	dataTree (M1 x) = DataTreeNode x
+	dataTree (M1 x) = dataTree x
 
 instance (DataTreeNode f1,DataTreeNode f2) => DataTreeNode (f1 :*: f2) where
-	dataTree (a :*: b) = DataTreeNode a ++ DataTreeNode b
+	dataTree (a :*: b) = DataTree "*" [dataTree a,dataTree b]
 
 instance (DataTreeNode f1,DataTreeNode f2) => DataTreeNode (f1 :+: f2) where
-	dataTree (L1 x) = DataTreeNode x
-	dataTree (R1 x) = DataTreeNode x
+	dataTree (L1 x) = DataTree "Left" [dataTree x]
+	dataTree (R1 x) = DataTree "Right" [dataTree x]
 
-toDataTree :: (Generic a,DataTreeNode (Rep a)) => a -> HTML
+toDataTree :: (Generic a,DataTreeNode (Rep a)) => a -> DataTree
 toDataTree x = dataTree (from x)
+
+dataTreeToHTML :: DataTree -> Html
+dataTreeToHTML datatree = noHtml
