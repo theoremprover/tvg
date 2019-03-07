54dae8f8919d577564349aac00087fb3f3a2d790
3T <none@none.com>
2019-03-07 17:30:27 +0100

    No Message

--- grammar/DataTree.hs
+++ grammar/DataTree.hs
@@ -12,32 +12,32 @@ import Text.Html
 data DataTree = DataTree String [DataTree] | Leaf String deriving (Show)
 
 class DataTreeNode f where
-	dataTree :: f p -> DataTree
+	dataTree :: f p -> [DataTree]
 
 instance DataTreeNode V1 where
 	dataTree _ = error "DataTreeNode of empty type!"
 
 instance {-# OVERLAPS #-} DataTreeNode (K1 i NodeInfo) where
-	dataTree (K1 c) = Leaf $ show c
+	dataTree (K1 c) = [Leaf $ show c]
 
 instance {-# OVERLAPS #-} DataTreeNode (K1 i Ident) where
-	dataTree (K1 (Ident name _ _)) = Leaf $ "Ident " ++ show name
+	dataTree (K1 (Ident name _ _)) = [Leaf $ "Ident " ++ show name]
 
 instance {-# OVERLAPS #-} DataTreeNode (K1 i Int) where
-	dataTree (K1 c) = Leaf $ show c
+	dataTree (K1 c) = [Leaf $ show c]
 instance {-# OVERLAPS #-} DataTreeNode (K1 i String) where
-	dataTree (K1 c) = Leaf $ show c
+	dataTree (K1 c) = [Leaf $ show c]
 instance {-# OVERLAPS #-} DataTreeNode (K1 i Char) where
-	dataTree (K1 c) = Leaf $ show c
+	dataTree (K1 c) = [Leaf $ show c]
 instance {-# OVERLAPS #-} DataTreeNode (K1 i Integer) where
-	dataTree (K1 c) = Leaf $ show c
+	dataTree (K1 c) = [Leaf $ show c]
 
 instance (Generic c,DataTreeNode (Rep c)) => DataTreeNode (K1 i c) where
 	dataTree (K1 c) = dataTree (from c)
 
 instance (DataTreeNode f,Constructor c) => DataTreeNode (M1 C c f) where
 	dataTree (M1 x) = case conName (undefined :: M1 C c f p) of
-		conname -> DataTree conname [dataTree x]
+		conname -> [DataTree conname (dataTree x)]
 
 instance (DataTreeNode f,Selector s) => DataTreeNode (M1 S s f) where
 	dataTree (M1 x) = dataTree x
@@ -46,10 +46,10 @@ instance (DataTreeNode f,Datatype d) => DataTreeNode (M1 D d f) where
 	dataTree (M1 x) = dataTree x
 
 instance DataTreeNode U1 where
-	dataTree (U1 p) = Leaf "()"
+	dataTree U1 = [Leaf $ "()"]
 
 instance (DataTreeNode f1,DataTreeNode f2) => DataTreeNode (f1 :*: f2) where
-	dataTree (a :*: b) = DataTree "*" [dataTree a,dataTree b]
+	dataTree (a :*: b) = dataTree a ++ dataTree b
 
 instance (DataTreeNode f1,DataTreeNode f2) => DataTreeNode (f1 :+: f2) where
 	dataTree (L1 x) = dataTree x
@@ -63,11 +63,11 @@ dataTreeToHtml (DataTree s subtrees) = li ((thespan ! [theclass "caret"]) (strin
 	(ulist ! [theclass "nested"]) (concatHtml $ map dataTreeToHtml subtrees))
 
 genericToHTMLString :: (Generic a,DataTreeNode (Rep a)) => a -> String
-genericToHTMLString x = renderHtml $ pageframe $ (! [identifier "myUL"]) $ dataTreeToHtml $ toDataTree x
+genericToHTMLString x = renderHtml $ pageframe $ map (! [identifier "myUL"]) $ dataTreeToHtml $ toDataTree x
 	where
-	pageframe tree_html =
+	pageframe tree_htmls =
 		header (style css) +++
-		body ( tree_html +++ tag "script" toggler )
+		body ( concatHtml tree_htmls +++ tag "script" toggler )
 
 css = primHtml "\
 \ ul, #myUL {\
