ebd482fa893500f31557141e6eef3cce4cbe9165
3T <none@none.com>
2019-03-19 08:58:06 +0100

    No Message

--- test.c
+++ test.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 
+int g(int x)
+{
+	if(x>5) return(2*x);
+	return(x+10);
+}
+
 int f(int x)
 {
 	int y = 2*x;
+	if(y>10)
+	{
+		y = y - 1;
+	}
+	else
+	{
+		y = 2*g(y);
+	}
 	return(y+1);
 }
 
