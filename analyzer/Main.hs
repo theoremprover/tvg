d261a91c49b15c43291956d3bbe933c16228f357
3T <none@none.com>
2019-03-27 17:22:34 +0100

    No Message

--- analyzer/Main.hs
+++ analyzer/Main.hs
@@ -3,13 +3,16 @@
 
 module Main where
 
-import Control.Monad
+import Control.Monad.Trans.State.Strict
 import System.Environment
 import Language.C
 import Language.C.Data.Ident
+import Language.C.Analysis.AstAnalysis
+import Language.C.Analysis.SemRep
 import Language.C.System.GCC
 import System.FilePath
 import Text.Printf
+import qualified Data.Map.Strict as Map
 
 import DataTree
 
@@ -21,21 +24,35 @@ stack build :analyzer-exe && stack exec analyzer-exe
 
 gccExe = "gcc-4.7"
 
+data CovVecState = CovVecState {
+	allDefs :: Map.Map Ident IdentDecl
+	}
+	
+type CovVecM a = StateT CovVecState IO a
+
 main = do
-	filename:functionname:[] <- return ["test.c","f"] --getArgs
+	filename:funname:[] <- return ["test.c","f"] --getArgs
 
 	mb_ast <- parseCFile (newGCC gccExe) Nothing [] filename
 	case mb_ast of
 		Left err -> error $ show err
 		Right translunit@(CTranslUnit extdecls _) -> do
 			writeFile (filename++".ast.html") $ genericToHTMLString translunit
-			let fundefs = concatMap ( \case 
-				CFDefExt fundef@(CFunDef _ (CDeclr (Just (Ident name _ _)) _ _ _ _) _ _ _) -> [(name,fundef)]
-				_ -> [] ) extdecls
-			print $ genCovVectors fundefs functionname
+			GlobalDecls globobjs _ _ <- analyseAST translunit
+			res <- evalStateT (genCovVectorsM (builtinIdent funname)) $ CovVecState globobjs
+			print res
 
 --data XXX = 
 
-genCovVectors fundefs funname = case lookup funname fundefs of
-	Nothing     -> error $ "Function " ++ funname ++ " not (uniquely) found"
-	Just fundef -> funname
+lookupFunM :: Ident -> CovVecM FunDef
+lookupFunM ident = do
+	map <- gets allDefs
+	case Map.lookup ident map of
+		Just (FunctionDef fundef) -> return fundef
+		_ -> error $ "Function " ++ (show ident) ++ " not found"
+
+genCovVectorsM :: Ident -> CovVecM NodeInfo
+genCovVectorsM funident = do
+	FunDef _ _ ni <- lookupFunM funident
+	return ni
+
