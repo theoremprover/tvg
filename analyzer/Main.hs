6ae6c661c039c67f6d1b42271d0d380bc41c516e
3T <none@none.com>
2019-04-17 10:09:58 +0200

    No Message

--- analyzer/Main.hs
+++ analyzer/Main.hs
@@ -40,7 +40,7 @@ main = do
 		Right translunit@(CTranslUnit extdecls _) -> do
 			writeFile (filename++".ast.html") $ genericToHTMLString translunit
 			let Right (GlobalDecls globobjs _ _,[]) = runTrav_ $ analyseAST translunit
-			res <- evalStateT (genCovVectorsM (builtinIdent funname) []) $ CovVecState globobjs
+			res <- evalStateT (genCovVectorsM [] (builtinIdent funname)) $ CovVecState globobjs
 			print res
 
 data Constraint = Or [Constraint] | And [Constraint] | Expr :<= Expr
@@ -53,28 +53,29 @@ lookupFunM ident = do
 		Just (FunctionDef fundef) -> return fundef
 		_ -> error $ "Function " ++ (show ident) ++ " not found"
 
-genCovVectorsM :: Ident -> [Constraint] -> CovVecM NodeInfo
-genCovVectorsM funident constraints = do
+genCovVectorsM :: [Stmt] -> Ident -> CovVecM [[Stmt]]
+genCovVectorsM tracepath funident = do
 	FunDef (VarDecl (VarName ident _) declattrs (FunctionType (FunType ret_type paramdecls False) _)) stmt ni <- lookupFunM funident
-	traces <- tracesStmtM [] stmt
-	return traces
+	tracesStmtM tracepath [stmt]
 
 tracesStmtM :: [Stmt] -> [Stmt] -> CovVecM [[Stmt]]
-tracesStmtM tracepath cexpr@(CExpr expr _) = case expr of
-	CAssign CAssignOp (CVar ident _) assigned_expr _ -> cexpr
-	CCall (CVar funname _) args _ -> -- Call function statement discarding result
+tracesStmtM tracepath ((stmt@(CExpr (Just expr) _)) : rest)= case expr of
+	CAssign assign_op (CVar ident _) assigned_expr _ -> tracesStmtM (stmt:tracepath) rest
+	CCall (CVar funident _) args _ -> genCovVectorsM tracepath funident
 	unknown -> error $ "traceStmtM CExpr: " ++ show unknown ++ " not implemented yet"
 tracesStmtM tracepath (CCompound _ cbis _ : rest) = tracesStmt tracepath (concatMap to_stmt cbis ++ rest) where
 	to_stmt (CBlockStmt cstmt) = [cstmt]
-	to_stmt (CBlockDecl (CDecl _ triples _)) = map triple_to_stmt triples
+	to_stmt (CBlockDecl (CDecl _ triples _)) = concatMap triple_to_stmt triples
+	triple_to_stmt (_,Nothing,Nothing) = []
 	triple_to_stmt (Just (CDeclr (Just ident) _ _ _ _),Just (CInitExpr init_expr _),Nothing) =
+		[ CExpr (Just $ CAssign CAssignOp (CVar ident undefNode) init_expr undefNode) undefNode ]
 	triple_to_stmt err = error $ "triple_to_stmt: " ++ show err ++ " not implemented yet"
-tracesStmtM tracepath (CIf cond_exp then_stmt mb_else_stmt _ : rest) = do
+tracesStmtM tracepath (CIf cond_expr then_stmt mb_else_stmt _ : rest) = do
 	then_branches <- tracesStmtM (CExpr (Just cond_expr) undefNode : tracepath) (then_stmt:rest)
 	else_branches <- case mb_else_stmt of
 		Just else_stmt -> tracesStmtM (CExpr (Just cond_expr) undefNode : tracepath) (else_stmt:rest)
-		Nothing -> []
+		Nothing -> return []
 	return $ then_branches ++ else_branches
-
-tracesStmtM tracepath cret@(CReturn (Just ret_expr) _) = return [cret : tracepath]
-tracesStmtM _ stmt = error $ "traceStmtM: " ++ show stmt ++ " not implemented yet"
+tracesStmtM tracepath (cret@(CReturn (Just ret_expr) _) : rest) = return [cret : tracepath]
+tracesStmtM tracepath [] = return [tracepath]
+tracesStmtM _ (stmt:_) = error $ "traceStmtM: " ++ show stmt ++ " not implemented yet"
