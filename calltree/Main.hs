5ca2da28e590a04fe1251cb35199b4672fcb02da
3T <none@none.com>
2019-05-25 02:05:18 +0200

    No Message

--- calltree/Main.hs
+++ calltree/Main.hs
@@ -28,7 +28,7 @@ main = do
 
 maini [] = putStrLn "USAGE (in tvg dir): stack exec :calltree-exe -- <GCC-EXE> <ROOT_FUN_NAME> {<SRCFILE>|<OPT>}*"
 
-maini (gccexe:rootfunname:args) = do
+maini (gccexe:args) = do
 	let (preprocess_args,other_args) = partition (\ arg -> any (`isPrefixOf` arg) ["-I","-D"]) args
 	pot_filess <- forM other_args getsrcfiles
 --	print pot_filess
@@ -38,13 +38,20 @@ maini (gccexe:rootfunname:args) = do
 
 	writeFile "graph.dot" $ unlines $ nub $ map (drawedge vertex2node) (edges callgraph)
 
-	forM_ (reachable callgraph $ fromJust (key2vertex rootfunname)) $ \ vertex -> do
-		let
-			calledfuns = map (\ v -> let (node,key,_) = vertex2node v in (node,key)) $ reachable callgraph vertex
-			((sourcefun,mb_sourcefile),_,_) = vertex2node vertex
-		writeFile ("calltree_out" </> sourcefun <.> "csv") $ printCSV $ ["FUNCTIONS CALLED DIRECTLY OR INDIRECTLY","FUNCTION DEFINED IN","DIRECTLY CALLED BY"] :
-			[ [f, maybe "<NOWHERE>" id mb_srcfile, concat $ intersperse ","
-				[ caller | (callerv,calledv) <- edges callgraph, Just calledv == key2vertex fk, let ((caller,_),_,_) = vertex2node callerv ] ] | ((f,mb_srcfile),fk) <- calledfuns ]
+	forM_ functions $ \ rootfunname -> do
+		case key2vertex rootfunname of
+			Nothing -> do
+				putStrLn $ "??? Function " ++ rootfunname ++ " not found in source files!"
+				writeFile ("calltree_out" </> (rootfunname ++ "_NOT_FOUND")) ""
+			Just rootvertex -> do
+				forM_ (reachable callgraph rootvertex) $ \ vertex -> do
+					let
+						calledfuns = map (\ v -> let (node,key,_) = vertex2node v in (node,key)) $ reachable callgraph vertex
+						((sourcefun,mb_sourcefile),_,_) = vertex2node vertex
+					createDirectoryIfMissing True ("calltree_out" </> rootfunname)
+					writeFile ("calltree_out" </> rootfunname </> sourcefun <.> "csv") $ printCSV $ ["FUNCTIONS CALLED DIRECTLY OR INDIRECTLY","FUNCTION DEFINED IN","DIRECTLY CALLED BY"] :
+						[ [f, maybe "<NOWHERE>" id mb_srcfile, concat $ intersperse ","
+							[ caller | (callerv,calledv) <- edges callgraph, Just calledv == key2vertex fk, let ((caller,_),_,_) = vertex2node callerv ] ] | ((f,mb_srcfile),fk) <- calledfuns ]
 
 	where
 	getsrcfiles :: String -> IO [String]
@@ -52,7 +59,7 @@ maini (gccexe:rootfunname:args) = do
 		isdir <- doesDirectoryExist name
 		case isdir of
 			False -> case any (`isSuffixOf` name) [".c",".h"] of
-				True -> putStrLn ("got srcfile " ++ name) >> return [name]
+				True -> return [name]
 				False -> return []
 			True -> do
 				names <- getDirectoryContents name
@@ -69,12 +76,10 @@ printCSV ls = unlines $ map (concat . (intersperse ";")) ls
 -- Returns a list of (funname,srcfile,[calledfunname1,...])
 handleSrcFile gccexe preprocess_args srcfilename = do
 	putStrLn $ "handling SrcFile " ++ srcfilename
-	mb_ast <- parseCFile (newGCC gccExe) Nothing preprocess_args srcfilename
+	mb_ast <- parseCFile (newGCC gccexe) Nothing preprocess_args srcfilename
 	case mb_ast of
 		Left err -> error $ show err
-		Right ctranslunit -> do
-			putStrLn $ "Parsed " ++ srcfilename
-			execStateT (everywhereM (mkM searchFunDefs) ctranslunit) []
+		Right ctranslunit -> execStateT (everywhereM (mkM searchFunDefs) ctranslunit) []
 			
 -- For all function definitons in the current source file:
 searchFunDefs :: CFunDef -> StateT [(String,Maybe FilePath,[String])] IO CFunDef
@@ -100,3 +105,8 @@ removeellipse filename = do
 	let ls = lines f
 	print $ length ls
 	writeFile filename $ unlines $ filter (\ l -> not ("static void __tg_promote(...)" `isInfixOf` l) && not (" __attribute__((__overloadable__, __unavailable__));" `isInfixOf` l)) ls
+
+functions = ["__aeabi_cdcmpeq","__aeabi_cdcmple","__aeabi_cdrcmple","__aeabi_cfcmpeq","__aeabi_cfcmple","__aeabi_cfrcmple","__aeabi_d2f","__aeabi_d2h","__aeabi_d2h_alt","__aeabi_d2iz","__aeabi_d2lz","__aeabi_d2uiz","__aeabi_d2ulz","__aeabi_dadd","__aeabi_dcmpeq","__aeabi_dcmpge","__aeabi_dcmpgt","__aeabi_dcmple","__aeabi_dcmplt","__aeabi_dcmpun","__aeabi_ddiv","__aeabi_dmul","__aeabi_drsub","__aeabi_dsub","__aeabi_errno_addr","__aeabi_f2d","__aeabi_f2h","__aeabi_f2h_alt","__aeabi_f2iz","__aeabi_f2lz","__aeabi_f2uiz","__aeabi_f2ulz","__aeabi_fadd","__aeabi_fcmpeq","__aeabi_fcmpge","__aeabi_fcmpgt","__aeabi_fcmple","__aeabi_fcmplt","__aeabi_fcmpun","__aeabi_fdiv","__aeabi_fmul","__aeabi_frsub","__aeabi_fsub","__aeabi_h2f","__aeabi_h2f_alt","__aeabi_i2d","__aeabi_i2f",
+	"__aeabi_idiv","__aeabi_idiv0","__aeabi_idivmod","__aeabi_l2d","__aeabi_l2f","__aeabi_lasr","__aeabi_lcmp","__aeabi_ldiv0","__aeabi_ldivmod","__aeabi_llsl","__aeabi_llsr","__aeabi_lmul","__aeabi_memclr","__aeabi_memclr4","__aeabi_memclr8","__aeabi_memcpy","__aeabi_memcpy4","__aeabi_memcpy8","__aeabi_memmove","__aeabi_memmove4","__aeabi_memmove8","__aeabi_memset","__aeabi_memset4","__aeabi_memset8","__aeabi_ui2d","__aeabi_ui2f","__aeabi_uidiv","__aeabi_uidivmod","__aeabi_ul2d","__aeabi_ul2f","__aeabi_ulcmp","__aeabi_uldivmod","__aeabi_uread4","__aeabi_uread8","__aeabi_uwrite4","__aeabi_uwrite8","_Atan","_Atan_divide","_Dint","_Dnorm","_Dscale","_Dscalex","_Dtest","_Dunscale","_Exp","_Expm_approx",
+	"_FDint","_FDnorm","_FDscale","_FDscalex","_FDtest","_FDunscale","_Feraise","_FExp","_FLog","_Force_raise","_FPmsw","_Getmem","_Hypot","_Locksyslock","_Log","_OK_Altab","_Pmsw","_Sbrk","_Tls_setup__Randinit","_Tls_setup__Randseed","_Tls_setup_idx","_Tls_setup_rv","_Tls_setup_ssave","_Unlocksyslock","_UPD_Altab","abs","acos","acosf","acosh","acoshf","add","addf","asin","asinf","asinh","asinhf","atan","atan2","atan2f","atanf","atanh","atanhf","cbrt","cbrtf","ceil","ceilf","cos","cosf","cosh","coshf","divide","dividef","exmp1f","exp","exp2","exp2f","expf","expm","expm1","fabs","fabsf","fdim","fdimf","fegetenv","feraiseexcept","fesetenv","findmem","floor","floorf","fmaf","fmax","fmaxf","fmin","fminf","fmod","fmodf","free","frexp","frexpf","HUGE_VAL","hypot","hypotf",
+	"ilogb","ilogbf","imaxabs","imaxdiv","ldexp","ldexpf","ldiv","llabs","lldiv","log","log10","log10f","log1p","log1pf","log2","log2f","logb","logbf","logf","logpf","Lrint","Lrintf","Lround","Lroundf","malloc","memcpy","memmove","modf","modff","multiply","multiplyf","myrealloc","nextafter","nextafterf","NULL","pow","powf","rand","realloc","remainder","remainderf","rint","rintf","round","roundf","scalbln","scalblnf","scalbn","scalbnf","sin","sinf","sinh","sinhf","size_t","sqrt","sqrtf","srand","strcspn","strspn","strtok","substract","substractf","tan","tanf","tanh","tanhf","trunc","truncf" ]
