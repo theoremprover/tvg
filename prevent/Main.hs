416183f0b761baba73644e945455b5caff3b683e
Theoremprover <no@email.com>
2019-06-10 17:21:42 +0200

    No Message

--- prevent/Main.hs
+++ prevent/Main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE RecordWildCards,LambdaCase,OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards,LambdaCase,OverloadedStrings,DeriveGeneric,StandaloneDeriving #-}
 {-# OPTIONS_GHC -fno-warn-tabs #-}
 
 module Main where
@@ -6,33 +6,25 @@ module Main where
 import System.Environment
 import Data.List
 import Control.Monad
-import Control.Monad.IO.Class
 import Language.C
-import Language.C.Data.Ident
 import Language.C.System.GCC
 import Language.C.Analysis.AstAnalysis
 import Language.C.Analysis.TravMonad
 import Language.C.Analysis.SemRep
-import Data.Generics
-import Control.Monad.Trans.State.Strict
-
+import Language.C.Data.Ident
+import GHC.Generics
 import System.FilePath
-import Text.Printf
 import qualified Data.Map.Strict as Map
-import Text.PrettyPrint
-
-import Control.Lens
-import CLenses
 
 import DataTree
 
 {--
-stack build :analyzer-exe
-stack exec analyzer-exe -- test.c
+stack build :prevent-exe
+stack exec prevent-exe -- test.c
 stack build :prevent-exe && stack exec prevent-exe -- prevent/test.c
 --}
 
-gccExe = "/usr/bin/gcc"
+gcc = newGCC "gcc"
 
 main = do
 	args <- getArgs
@@ -42,15 +34,36 @@ maini args = do
 	let preprocess_args = filter (\ arg -> any (`isPrefixOf` arg) ["-I","-D"]) args
 	forM (filter (".c" `isSuffixOf`) args) (handleSrcFile preprocess_args)
 
+--deriving instance Generic GlobalDecls
+deriving instance Generic IdentDecl
+deriving instance Generic Decl
+deriving instance Generic ObjDef
+deriving instance Generic FunDef
+deriving instance Generic Enumerator
+deriving instance Generic VarDecl
+deriving instance Generic VarName
+deriving instance Generic DeclAttrs
+deriving instance Generic FunctionAttrs
+deriving instance Generic Storage
+deriving instance Generic Linkage
+deriving instance Generic Attr
+deriving instance Generic Type
+deriving instance Generic TypeName
+deriving instance Generic IntType
+
+
+{-
+data GlobalDecls deriving Generic
+data Map deriving Generic
+data Ident deriving Generic
+data IdentDecl deriving Generic
+-}
+
 handleSrcFile preprocess_args srcfilename = do
-	mb_ast <- parseCFile (newGCC gccExe) Nothing preprocess_args srcfilename
+	mb_ast <- parseCFile gcc Nothing preprocess_args srcfilename
 	case mb_ast of
 		Left err -> error $ show err
 		Right ctranslunit -> do
-			writeFile (srcfilename++".ast.html") $ genericToHTMLString ctranslunit
---			let rets search ctranslunit
---			forM_ rets print
---			return rets
-
---search :: CTranslationUnit NodeInfo -> [String]
---search ctranslunit = map (render.pretty) $ toListOf $ ctranslunit
+--			writeFile (srcfilename++".ast.html") $ genericToHTMLString ctranslunit
+			let Right (GlobalDecls globobjs _ _,[]) = runTrav_ $ analyseAST ctranslunit
+			writeFile (srcfilename <.> "ast" <.> "html") $ genericToHTMLString (Map.toList globobjs)
