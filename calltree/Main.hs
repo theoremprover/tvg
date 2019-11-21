0e31385ccbea8e2d03b51f8a2a61c0fb6fe20333
none <none@none.com>
2019-11-21 16:27:46 +0100

    ii

--- calltree/Main.hs
+++ calltree/Main.hs
@@ -2,7 +2,7 @@
 {-# OPTIONS_GHC -fno-warn-tabs #-}
 
 {-
-stack build :calltree-exe && stack exec calltree-exe -- "gcc" calltreetest.c calltreetest2.c calltree.h
+stack build :calltree-exe && stack exec calltree-exe -- "gcc" <SRCDIR>
 -}
 
 module Main where
@@ -34,7 +34,7 @@ maini [] = do
 maini (gccexe:args) = do
 	let (preprocess_args,other_args) = partition (\ arg -> any (`isPrefixOf` arg) ["-I","-D"]) args
 	pot_filess <- forM other_args getsrcfiles
---	mapM_ print (concat pot_filess)
+	mapM_ print (concat pot_filess)
 	callss <- forM (concat pot_filess) (handleSrcFile gccexe preprocess_args)
 --	forM_ (concat callss) print
 
@@ -102,8 +102,8 @@ removeellipses path = do
 		print $ length ls
 		writeFile filename $ unlines $ ls
 
-functions = [
-	"dl_main","dl_start","dl_start_final" ]
+functions = [ "f" ]
+	--"dl_main","dl_start","dl_start_final" ]
 
 printCSV ls = unlines $ map (concat . (intersperse ";")) ls
 
@@ -114,21 +114,42 @@ handleSrcFile gccexe preprocess_args srcfilename = do
 --	mb_ast <- parseCFilePre srcfilename
 	case mb_ast of
 		Left err -> error $ show err
-		Right ctranslunit -> execStateT (everywhereM (mkM searchFunDefs) ctranslunit) []
+		Right ctranslunit -> execStateT (everywhereM (mkM (searchFunDefs ctranslunit)) ctranslunit) []
 
 -- For all function definitons in the current source file:
-searchFunDefs :: CFunDef -> StateT [(String,Maybe FilePath,[String])] IO CFunDef
-searchFunDefs cfundef@(CFunDef _ (CDeclr (Just (Ident funname _ _)) _ _ _ _) _ stmt ni) = do
-	calledfunnames <- execStateT (everywhereM (mkM searchFunCalls) stmt) []
+searchFunDefs :: CTranslUnit -> CFunDef -> StateT [(String,Maybe FilePath,[String])] IO CFunDef
+searchFunDefs ctranslunit cfundef@(CFunDef _ (CDeclr (Just (Ident funname _ _)) (CFunDeclr (Right (argdecls,_)) _ _ : _) _ _ _) _ stmt ni) = do
+	calledfunnames <- execStateT (everywhereM (mkM $ searchFunCalls funname argdecls ctranslunit) stmt) []
 --	liftIO $ print calledfunnames
 	modify ((funname,fileOfNode ni,nub calledfunnames) : )
 	return cfundef
 
 -- Returns all directly called functions
-searchFunCalls :: (MonadIO m) => CExpr -> StateT [String] m CExpr
-searchFunCalls ccall@(CCall funname_expr _ ni) = case funname_expr of
+searchFunCalls :: (MonadIO m) => String -> [CDecl] -> CTranslUnit -> CExpr -> StateT [String] m CExpr
+searchFunCalls currentfunname argdecls ctranslunit ccall@(CCall funname_expr callargs ni) = case funname_expr of
 	CVar (Ident name _ _) _ -> do
 		modify (name : )
+		return ccall
+	CUnary CIndOp (CVar (Ident name _ _) _) _ -> do
+		liftIO $ do
+			putStrLn $ "ccall = " ++ (render.pretty) ccall
+			putStrLn $ "currentfunname = " ++ currentfunname
+			putStrLn $ "Ident name = " ++ name
+			putStrLn $ "argdecls = "
+			forM_ argdecls print
+		let
+			[(argnum,_)] = filter (\ (_,CDecl _ [(Just (CDeclr (Just (Ident argname _ _)) _ _ _ _),_,_)] _) -> argname==name) (zip [0..] argdecls)
+			calls :: [CExpr]
+			calls = everything (++) (mkQ [] searchfundefs) ctranslunit
+			searchfundefs :: CFunDef -> [CExpr]
+			searchfundefs (CFunDef _ (CDeclr (Just (Ident funname _ _)) _ _ _ _) _ stmt _) = everything (++) (mkQ [] searchfuncalls) stmt
+			searchfundefs _ = []
+			searchfuncalls :: CExpr -> [CExpr]
+			searchfuncalls (CCall (CVar (Ident calledfunname _ _) _) args _) | calledfunname==currentfunname = [ args!!argnum ]
+			searchfuncalls _ = []
+			names = map (\ (CVar (Ident fn _ _) _) -> fn) calls
+		modify (names++)
+		liftIO $ putStrLn $ "Found funptr calls: " ++ show ni ++ " " ++ show names
 		return ccall
 	fne -> do
 		let msg = show (posOfNode ni) ++ ": " ++ (render $ pretty ccall) ++ "\n"
@@ -137,5 +158,5 @@ searchFunCalls ccall@(CCall funname_expr _ ni) = case funname_expr of
 			appendFile ("calltree_out" </> "strangecalls.txt") msg 
 		return ccall
 
-searchFunCalls cexpr = return cexpr
+searchFunCalls _ _ _ cexpr = return cexpr
 
