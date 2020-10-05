eb00c3d467920c5f1af481e39872364100db9c54
Reitmeier <reitmeier@validas.de>
2020-10-05 11:21:21 +0200

    sda

--- init_env.sh
+++
@@ -1,5 +0,0 @@
-export CC="/home/robert/.local/bin/tvg-exe /tvg"
-export CFLAGS="-w -I/usr/include/x86_64-linux-gnu"
-export LDFLAGS="" #-L/usr/lib/x86_64-linux-gnu"
-export LD_LIBRARY_PATH=/tvg/tvg/incs
-export LIBRARY_PATH=/usr/lib/x86_64-linux-gnu
