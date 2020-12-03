28c7ac64b50045fa34ef7a1f553cd2dbe5268d47
Reitmeier <reitmeier@validas.de>
2020-12-03 18:15:40 +0100

    sweemstoworkfsdsadewrsdsadsafds

--- analyzer/floattest.c
+++ analyzer/floattest.c
@@ -1,7 +1,7 @@
 float f(float x,int n)
 {
-    float xe = 1.0;
-    for(int i=0;solver_pragma(3) && i<n;i++)
+    float xe = 2.0;
+//    for(int i=0;solver_pragma(3) && i<n;i++)
     {
         xe *= x;
     }
