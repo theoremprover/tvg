d7b4e82e4e657e59832072b85bed130afb10e617
theoremprover <none@none.com>
2021-01-23 00:08:31 +0100

    OK

--- analyzer/OscarsChallenge/sin/xdtest.c
+++ analyzer/OscarsChallenge/sin/xdtest.c
@@ -11,7 +11,7 @@ typedef union
 short _Dtest(double *px)
  {
  _Dval *ps = (_Dval *)(char *)px;
-  
+
  if ((ps->_Sh[3] & ((unsigned short)(0x7fff & ~((unsigned short)((1 << 4) - 1))))) == ((unsigned short)((1 << (15 - 4)) - 1)) << 4)
   return ((short)((ps->_Sh[3] & ((unsigned short)((1 << 4) - 1))) != 0 || ps->_Sh[2] != 0));
   return (0);
