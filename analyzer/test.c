14c6722055fdf6af0c2c6bad37c12b5640d71449
theoremprover <none@none.com>
2021-02-22 20:37:51 +0100

    Checkpoint OK

--- analyzer/test.c
+++ analyzer/test.c
@@ -4,8 +4,16 @@ typedef union
  double _Val;
  } __attribute__ ((__may_alias__)) _Dval;
 
-short f(double *px)
+short _Dtest(double *px)
  {
  _Dval *ps = (_Dval *)(char *)px;
- return (ps->_Sh[0]);
-}
+
+ if ((ps->_Sh[3] & ((unsigned short)(0x7fff & ~((unsigned short)((1 << 4) - 1))))) == ((unsigned short)((1 << (15 - 4)) - 1)) << 4)
+  return ((short)((ps->_Sh[3] & ((unsigned short)((1 << 4) - 1))) != 0 || ps->_Sh[2] != 0
+   || ps->_Sh[1] != 0 || ps->_Sh[0] != 0 ? 2 : 1));
+ else if ((ps->_Sh[3] & ~(((_Dval *)(char *)&(*px))->_Sh[3] & ((unsigned short)0x8000))) != 0 || ps->_Sh[2] != 0
+  || ps->_Sh[1] != 0 || ps->_Sh[0] != 0)
+  return ((ps->_Sh[3] & ((unsigned short)(0x7fff & ~((unsigned short)((1 << 4) - 1))))) == 0 ? (-2) : (-1));
+ else
+  return (0);
+ }
