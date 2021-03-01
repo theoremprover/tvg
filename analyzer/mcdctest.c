5a3a7d7b103c1bd0973d98a098e828b649d1d3c6
Reitmeier <reitmeier@validas.de>
2021-03-01 16:45:15 +0100

    reverted to c129dee

--- analyzer/mcdctest.c
+++ analyzer/mcdctest.c
@@ -2,9 +2,8 @@ int f(double x,double y,int z)
 {
     if(x>0.0 || y<0.0)
     {
-        return 1; //(z==1 ? 1 : -1);
+        return (z==1 ? 1 : -1);
     }
-    return 0;
 
     for(int i=0;i<3;i++) z++;
 
