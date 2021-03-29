792064f91a6d7e8a811fa5ace39b08aba1101a8c
Reitmeier <reitmeier@validas.de>
2021-03-29 16:29:22 +0200

    Before translExpr refactoring

--- analyzer/test.c
+++ analyzer/test.c
@@ -38,14 +38,6 @@ int __attribute__((fardata)) _Errno;
 }
 
 
-/*
-C:\Data\tvg\analyzer>main.exe 10 0
-_FDnorm(ps->_Sh[0] = 10, ps->_Sh[1] = 0, f2u(ps->_Val) = a) =
-1.1.xchar=-15
-2.1.xchar=-19
-2.2.xchar=-19
--19
-*/
 short _FDnorm(_Fval *ps)
  {
  short xchar;
