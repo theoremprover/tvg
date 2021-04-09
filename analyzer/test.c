45ec584a2fa6c2266be1ebb9ef505c372f598ee0
Reitmeier <reitmeier@validas.de>
2021-04-09 15:36:47 +0200

    Before translExpr refactoring

--- analyzer/test.c
+++ analyzer/test.c
@@ -52,13 +52,13 @@ short _FDnorm(_Fval *ps)
   {
   if (ps->_Sh[1] == 0)
     ps->_Sh[1] = ps->_Sh[0], ps->_Sh[0] = 0, xchar -= 16;
-  for (;solver_pragma(1,2) && (ps->_Sh[1] < 1 << 7); --xchar)
+  for (;solver_pragma(12,12,12,12,2) && (ps->_Sh[1] < 1 << 7); --xchar)
    {
    ps->_Sh[1] = (unsigned short)(ps->_Sh[1] << 1
     | ps->_Sh[0] >> 15);
    ps->_Sh[0] <<= 1;
    }
-  for (;solver_pragma(1,2) && (1 << (7 + 1) <= ps->_Sh[1]); ++xchar)
+  for (;solver_pragma(2) && (1 << (7 + 1) <= ps->_Sh[1]); ++xchar)
    {
    ps->_Sh[0] = (unsigned short)(ps->_Sh[0] >> 1
     | ps->_Sh[1] << 15);
