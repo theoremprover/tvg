1c8d812ffa167f282c83cdb0676a576770694621
Reitmeier <reitmeier@validas.de>
2022-02-02 16:10:37 +0100

    101

--- analyzer/hightecconti/my_df_to_sf.c
+++ analyzer/hightecconti/my_df_to_sf.c
@@ -2,13 +2,80 @@ __attribute__((__cdecl__)) int printf(const char *, ...);
 __attribute__((__cdecl__)) int sscanf(const char *, const char *, ...);
 
 #ifdef MAN
-int solver_pragma(int x, ...)
+union { float float_val; unsigned long int uint_val; } float_conv;
+union { double double_val; unsigned long long int ulong_val; } double_conv;
+float u2f(unsigned long u)
 {
-return 1;
+float_conv.uint_val = u;
+return float_conv.float_val;
 }
+double u2d(unsigned long long u)
+{
+double_conv.ulong_val = u;
+return double_conv.double_val;
+}
+unsigned long f2u(float f)
+{
+float_conv.float_val = f;
+return float_conv.uint_val;
+}
+unsigned long long d2u(double f)
+{
+double_conv.double_val = f;
+return double_conv.ulong_val;
+}
+void solver_debug_Float(char *s, float x)
+{
+printf("DEBUG_VAL Float %s = %g = 0x%lx\n", s, x, f2u(x));
+}
+void solver_debug_Double(char *s, double x)
+{
+printf("DEBUG_VAL Double %s = %g = 0x%llx\n", s, x, d2u(x));
+}
+void solver_debug_UByte(char *s, unsigned char x)
+{
+printf("DEBUG_VAL UByte %s = %hhi = 0x%hhx\n", s, x, x);
+}
+void solver_debug_Short(char *s, short x)
+{
+printf("DEBUG_VAL Short %s = %hi = 0x%hx\n", s, x, x);
+}
+void solver_debug_UShort(char *s, unsigned short x)
+{
+printf("DEBUG_VAL UShort %s = %hu = 0x%hx\n", s, x, x);
+}
+void solver_debug_UInt(char *s, unsigned int x)
+{
+printf("DEBUG_VAL UInt %s = %u = 0x%x\n", s, x, x);
+}
+void solver_debug_Int(char *s, int x)
+{
+printf("DEBUG_VAL_Int %s = %i = 0x%lx\n", s, x, x);
+}
+void solver_debug_ULong(char *s, unsigned long x)
+{
+printf("DEBUG_VAL ULong %s = %lu = 0x%lx\n", s, x, x);
+}
+void solver_debug_Long(char *s, long x)
+{
+printf("DEBUG_VAL Long %s = %li = 0x%lx\n", s, x, x);
+}
+void solver_debug_ULongLong(char *s, unsigned long long x)
+{
+printf("DEBUG_VAL ULongLong %s = %llu = 0x%llx\n", s, x, x);
+}
+void solver_debug_LongLong(char *s, long long x)
+{
+printf("DEBUG_VAL LongLong %s = %lli = 0x%llx\n", s, x, x);
+}
+
 void solver_find(int i)
 {
 printf("solver_find() encountered! %i\n",i);
+}
+int solver_pragma(int x, ...)
+{
+return 1;
 }
 #endif
 
@@ -234,69 +301,78 @@ __pack_f (const fp_number_type_s *src)
       exp = (0xff);
       fraction = ((fractype) 1 << 23) - 1;
     }
-  else if (solver_pragma(2) && isnan_s (src))
+  else if (isnan_s (src))
     {
       exp = (0xff);
 
       fraction >>= 7L;
       fraction &= 0x400000L - 1;
       if (src->class == CLASS_QNAN || 1)
- {
-   fraction |= 0x400000L;
-
- }
+	 {
+	   fraction |= 0x400000L;
+	 }
     }
-  else if (solver_pragma(2) && isinf_s (src))
+  else if (isinf_s (src))
     {
       exp = (0xff);
       fraction = 0;
     }
-  else if (solver_pragma(2) && iszero_s (src))
+  else if (iszero_s (src))
     {
       exp = 0;
       fraction = 0;
     }
   else if (fraction == 0)
     {
+        solver_find(1);
       exp = 0;
     }
   else
     {
       if (__builtin_expect (src->normal_exp < (-(127)+1), 0))
- {
-   int shift = (-(127)+1) - src->normal_exp;
+	 {
+	   int shift = (-(127)+1) - src->normal_exp;
 
-   exp = 0;
+	   exp = 0;
 
-   if (shift > 32 - 7L)
-     {
+	   if (shift > 32 - 7L)
+	     {
 
-       fraction = 0;
-     }
-   else
-     {
-       int lowbit = (fraction & (((fractype)1 << shift) - 1)) ? 1 : 0;
-       fraction = (fraction >> shift) | lowbit;
-     }
-   if ((fraction & 0x7f) == 0x40)
-     {
-       if ((fraction & (1 << 7L)))
-  fraction += 0x3f + 1;
-     }
-   else
-     {
+	       fraction = 0;
+	     }
+	   else
+	     {
+	       int lowbit = (fraction & (((fractype)1 << shift) - 1)) ? 1 : 0;
+	       fraction = (fraction >> shift) | lowbit;
+	     }
+	   if ((fraction & 0x7f) == 0x40)
+	     {
+	       if ((fraction & (1 << 7L)))
+	       {
+	            fraction += 0x3f + 1;
+	             solver_find(2);
+	       }
+	       else
+	       {
+	              solver_find(3);
+	       }
+	     }
+	   else
+	     {
 
-       fraction += 0x3f;
-     }
+	       fraction += 0x3f;
+	     }
 
 
-   if (fraction >= ((fractype)1<<(23 +7L)))
-     {
-       exp += 1;
-     }
-   fraction >>= 7L;
+	   if (fraction >= ((fractype)1<<(23 +7L)))
+	     {
+	        fraction >>= 1;
+	        solver_find(4);
+	       exp += 1;
+	     }
+	   fraction >>= 7L;
 
- }
+	 }
       else if (__builtin_expect (src->normal_exp > 127, 0))
  {
    exp = (0xff);
@@ -310,7 +386,14 @@ __pack_f (const fp_number_type_s *src)
 		  if ((fraction & 0x7f) == 0x40)
 		  {
 		    if (fraction & (1 << 7L))
+		    {
 		      fraction += 0x3f + 1;
+              solver_find(5);
+		    }
+		    else
+		    {
+                 solver_find(6);
+		    }
 		  }
 		       else
 		  {
@@ -325,7 +408,7 @@ __pack_f (const fp_number_type_s *src)
      }
    fraction >>= 7L;
 
-   if (solver_pragma(2) && exp > (0xff))
+   if (0 && exp > (0xff))
      {
        exp = (0xff);
        fraction = ((fractype) 1 << 23) - 1;
@@ -375,3 +458,50 @@ __truncdfsf2 (DFtype arg_a)
 
   return __make_fp (fp1->class, fp1->sign, fp1->normal_exp, sffrac);
 }
+
+#ifdef MAN
+void main(void)
+{
+	FLO_union_type f0a;
+	unsigned long long int ds[] = {
+//		 0x0008000000000001ull ,
+//		 0x0008000000000000ull ,
+//		 0x0004000000000001ull ,
+//		 0x0004000000000000ull ,
+//		 0x0010000000000001ull ,
+//		 0x380fffffe0000001ull ,
+//		 0x36bc000008000008ull ,
+//		 0x7270000000000001ull ,
+//		 0x381ffffff0000001ull ,
+//		 0x3820305c20000001ull ,
+//	10	 0x0010000000000000ull ,
+//		 0x380fffffe0c00000ull ,
+//		 0x376bef0002000000ull ,
+		 0x380fffffe0000000ull , // solver_find(2,4
+//		 0x38000000e0000000ull ,
+		 0x3690000000000000ull ,   // solver_find(3
+//		 0x380fffffe0800000ull ,
+//		 0x375bc4c000000000ull ,
+//		 0x6000000000000000ull ,
+		 0x381ffffff0000000ull ,  // solver_find(5)
+//		 0x3a01082bf0000000ull ,
+		 0x3810000010000000ull   // solver_find(6)
+//		 0x419ffffff0400000ull ,
+//		 0x3a00750860000000ull
+	};
+	for(int i=0;i<sizeof(ds)/sizeof(ds[0]);i++)
+	{
+		printf("##### %i\n",i);
+		f0a.raw_value = ds[i];
+		__truncdfsf2(f0a.value);
+	}
+
+}
+#endif
+
+/*
+		 0x380fffffe0000000ull , // solver_find(2,4)
+		 0x3690000000000000ull ,   // solver_find(3)
+		 0x381ffffff0000000ull ,  // solver_find(5)
+		 0x3810000010000000ull   // solver_find(6)
+*/
