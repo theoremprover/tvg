e06a244cb16634c11f1305051f8bc9d9062a3f7b
Reitmeier <reitmeier@validas.de>
2022-02-04 15:46:42 +0100

    101

--- analyzer/hightecconti/drilldown.c
+++ analyzer/hightecconti/drilldown.c
@@ -339,9 +339,9 @@ __unpack_d_drill (FLO_union_type * src, fp_number_type * dst)
   sign = (src->raw_value) >> 63 ;
 
   dst->sign = sign;
-  if (exp == 0)
+  if (solver_pragma(2,2) && exp == 0)
     {
-         if (solver_pragma(2,2) && fraction == 0)
+         if (fraction == 0)
          {
            dst->class = CLASS_ZERO;
          }
