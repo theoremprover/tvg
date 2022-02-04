401b503e82a3cd08e2dad2b8c9ea65110396a6b0
theoremprover <none@none.com>
2022-02-04 23:44:42 +0100

    vorREF

--- analyzer/Main.hs
+++ analyzer/Main.hs
@@ -112,7 +112,7 @@ printTypes = False
 printLocations = False
 errorModelPath = analyzerPath </> "models"
 
-mAX_UNROLLS = 1
+mAX_UNROLLS = 3
 uNROLLING_STRATEGY = [0..mAX_UNROLLS]
 
 sizeConditionChunks = 4
